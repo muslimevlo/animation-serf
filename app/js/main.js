a01ba3635cdd7d6555c371247f73f6cd2a21ffbf
muslimevlo <muslimevlo@yandex.ru>
2019-10-25 19:29:15 +0300

    serf-slider

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,14 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
-    })
+    });
+
+    $('.serf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+    });
 
 
   
