10526850ba75ec9e0d33ee088cfd0c4143909a00
muslimevlo <muslimevlo@yandex.ru>
2019-10-27 02:15:55 +0300

    блок travel готов

--- app/js/main.js
+++ app/js/main.js
@@ -5,10 +5,10 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
-        asNavFor: '.slider-dots',
+        asNavFor: '.slider-dotshead',
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
@@ -19,8 +19,22 @@ $(function(){
         slidesToScroll: 1,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+        asNavFor: '.slider-map',
     });
 
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.serf-slider',
+        focusOnSelect: true,
+    });
 
-  
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+    });
+    
 });
